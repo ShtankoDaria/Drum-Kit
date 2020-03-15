40e6b04b7115fb5d029d1af4f2bdfc39e766d4a4
ShtankoDaria <59530782+ShtankoDaria@users.noreply.github.com>
2020-03-15 19:21:37 +0100

    Update drum-kit.js

--- drum-kit.js
+++ drum-kit.js
@@ -1,3 +1,4 @@
+window.addEventListener("keydown", playSound);
 function playSound(e) {
   const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
   const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
@@ -15,4 +16,4 @@ function removeTransition(e) {
 
 const keys = document.querySelectorAll(".key");
 keys.forEach(key => key.addEventListener("transitionend", removeTransition));
-window.addEventListener("keydown", playSound);
+
